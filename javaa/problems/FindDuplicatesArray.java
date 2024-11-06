642a02c68c1c375769158e654e96002e4481056a
Renish B <renishb10@gmail.com>
2024-11-06 16:23:28 +0530

    update findDuplicatesArray

--- javaa/problems/FindDuplicatesArray.java
+++ javaa/problems/FindDuplicatesArray.java
@@ -1,27 +1,27 @@
 package javaa.problems;
 
-import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FindDuplicatesArray {
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 3, 4};
-        int[] arr2 = {5, 0, 6, 9, 7};
+        int[] arr2 = {5, 4, 6, 9, 7};
 
-        // O(n) solution using HashMap
+        // O(n) solution using HashSet
+        // O(a) Space complexity, we create new HashSet
         Boolean result = containsDuplicate(arr1, arr2);
         System.out.println(result ? "Array contains Duplicate" : "Array is Unique");
     }
 
     private static Boolean containsDuplicate(int[] arr1, int[] arr2) {
-        HashMap<Integer, Boolean> map = new HashMap<Integer, Boolean>();
+        Set<Integer> items = new HashSet<Integer>();
         for (int i : arr1) {
-            if (!map.containsKey(i)) {
-                map.put(i, true);
-            }
+            items.add(i);
         }
 
         for (int j : arr2) {
-            if (map.containsKey(j)) {
+            if (items.contains(j)) {
                 return true;
             }
         }
