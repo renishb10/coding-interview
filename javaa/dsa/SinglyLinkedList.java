d01cf5438a8b70de6eaaf84db424432871249d88
Renish B <renishb10@gmail.com>
2024-11-08 23:37:14 +0530

    sll append and prepend

--- javaa/dsa/SinglyLinkedList.java
+++ javaa/dsa/SinglyLinkedList.java
@@ -9,29 +9,42 @@ class Node {
 
     int data;
     Node next;
-    int size;
 }
 
 public class SinglyLinkedList {
     public Node head = null;
     public Node tail = null;
+    public int size = 0;
 
     public void prepend(int data) {
         Node newNode = new Node(data);
         newNode.next = head;
         head = newNode;
+
+        if (tail == null) tail = newNode;
+
+        size++;
     }
 
     public void append(int data) {
         Node newNode = new Node(data);
-        newNode.next = head;
-        head = newNode;
+        
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } 
+        else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        size++;
     }
 
     public void print() {
         Node currNode = head;
         while(currNode != null) {
-            String strFormat = currNode.next != null ?  "%d -> " : "%d -> null";
+            String strFormat = currNode.next != null ?  "%d -> " : "%d -> null\n";
             System.out.printf(strFormat, currNode.data);
             currNode = currNode.next;
         }
@@ -42,6 +55,11 @@ public class SinglyLinkedList {
         slist.prepend(1);
         slist.prepend(2);
         slist.prepend(3);
+        slist.append(0);
+        slist.append(-1);
         slist.print();
+        System.out.println("Head is " + slist.head.data);
+        System.out.println("Tail is " + slist.tail.data);
+        System.out.println("Size is " + slist.size);
     }
 }
