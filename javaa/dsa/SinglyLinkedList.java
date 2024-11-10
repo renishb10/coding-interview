c0bcbf4df7c411c01e51b08b891ae897bcec1902
Renish B <renishb10@gmail.com>
2024-11-10 17:24:13 +0530

    sll insertAfter

--- javaa/dsa/SinglyLinkedList.java
+++ javaa/dsa/SinglyLinkedList.java
@@ -41,6 +41,40 @@ public class SinglyLinkedList {
         size++;
     }
 
+    public void insertAfter(int pos, int data) {
+        if (pos < 1 || pos > size) {
+            System.out.println("Index out of bounds");
+            return;
+        }
+    
+        // If inserting at the beginning, use prepend
+        if (pos == 1) {
+            prepend(data);
+            return;
+        }
+    
+        // If inserting at the end, use append
+        if (pos == size) {
+            append(data);
+            return;
+        }
+    
+        Node currNode = head;
+        for (int i = 1; i < pos; i++) {  // Iterate to the node before the insertion point
+            currNode = currNode.next;
+        }
+    
+        Node newNode = new Node(data);
+        newNode.next = currNode.next;
+        currNode.next = newNode;
+        size++;
+    
+        // Update tail if we inserted at the end
+        if (newNode.next == null) {
+            tail = newNode;
+        }
+    }
+
     public void print() {
         Node currNode = head;
         while(currNode != null) {
@@ -57,6 +91,16 @@ public class SinglyLinkedList {
         slist.prepend(3);
         slist.append(0);
         slist.append(-1);
+        slist.print();
+        System.out.println("Head is " + slist.head.data);
+        System.out.println("Tail is " + slist.tail.data);
+        System.out.println("Size is " + slist.size);
+
+        slist.insertAfter(2, 99);
+        slist.insertAfter(1, 22);
+        slist.insertAfter(7, 101);
+        slist.insertAfter(10, 11);
+
         slist.print();
         System.out.println("Head is " + slist.head.data);
         System.out.println("Tail is " + slist.tail.data);
