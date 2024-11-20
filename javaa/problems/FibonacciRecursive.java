3edbce3fed1ef2429a623bb06990add35b57ed91
Renish B <renishb10@gmail.com>
2024-11-20 12:19:55 +0530

    fibonacci

---
+++ javaa/problems/FibonacciRecursive.java
@@ -0,0 +1,65 @@
+package javaa.problems;
+
+public class FibonacciRecursive {
+
+    public static int findFibonacciRecursive(int num) {
+        if (num < 2)
+            return num;
+
+        return findFibonacciRecursive(num - 1) + findFibonacciRecursive(num - 2);
+    }
+
+    public static int findFibonacciIterative(int num) {
+        if (num < 0) {
+            throw new IllegalArgumentException("Fibonacci is undefined for negative numbers.");
+        }
+        if (num == 0)
+            return 0;
+        if (num == 1)
+            return 1;
+
+        int prev1 = 1, prev2 = 0, curr = 0;
+        for (int i = 2; i <= num; i++) {
+            curr = prev1 + prev2; // Fibonacci formula: F(n) = F(n-1) + F(n-2)
+            prev2 = prev1; // Move prev1 to prev2
+            prev1 = curr; // Move curr to prev1
+        }
+        return curr;
+    }
+
+    public static int findFibonacciIterative2(int num) {
+        if (num < 0) {
+            throw new IllegalArgumentException("Fibonacci is undefined for negative numbers.");
+        }
+        if (num == 0) {
+            return 0;
+        }
+        if (num == 1) {
+            return 1;
+        }
+
+        int[] series = new int[num + 1];
+        series[0] = 0;
+        series[1] = 1;
+
+        for (int i = 2; i <= num; i++) {
+            series[i] = series[i - 2] + series[i - 1];
+        }
+
+        return series[num];
+    }
+
+
+    public static void main(String[] args) {
+        // 0, 1, 1, 2, 3, 5, 8, 13, 21 = 2
+        System.out.println(findFibonacciRecursive(3));
+        System.out.println(findFibonacciRecursive(0));
+        System.out.println(findFibonacciRecursive(8));
+        System.out.println(findFibonacciIterative(3));
+        System.out.println(findFibonacciIterative(0));
+        System.out.println(findFibonacciIterative(8));
+        System.out.println(findFibonacciIterative2(3));
+        System.out.println(findFibonacciIterative2(0));
+        System.out.println(findFibonacciIterative2(8));
+    }
+}
