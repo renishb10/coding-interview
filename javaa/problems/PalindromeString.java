6cdac2854621650b71076474f18f9aa5fd0eeb8c
Renish B <renishb10@gmail.com>
2025-01-07 11:47:10 +0530

    simple palindrom string

---
+++ javaa/problems/PalindromeString.java
@@ -0,0 +1,33 @@
+package javaa.problems;
+
+public class PalindromeString {
+    private static boolean isPalindrome(String s) {
+        if (s == null || s.length() <= 1) {
+            return true;
+        }
+
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            // Skip non-alphanumeric characters
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            // Compare characters case-insensitively
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("madam"));
+        System.out.println(isPalindrome("world"));
+    }
+}
