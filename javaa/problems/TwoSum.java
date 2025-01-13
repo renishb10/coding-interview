c409a7223c7c6e8145bbe22bff3a8e3ea325b93b
Renish B <renishb10@gmail.com>
2025-01-13 11:20:11 +0530

    two sum return indexes

--- javaa/problems/TwoSum.java
+++ javaa/problems/TwoSum.java
@@ -1,12 +1,16 @@
 package javaa.problems;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 public class TwoSum {
     public static void main(String[] args) {
-        System.out.println(hasTwoSumPair(new int[]{2,1,9,6,3}, 7));
-        System.out.println(hasTwoSumPair(new int[]{1,7,8,4,3}, 6));
+        System.out.println(hasTwoSumPair(new int[] {2, 1, 9, 6, 3}, 7));
+        System.out.println(hasTwoSumPair(new int[] {1, 7, 8, 4, 3}, 6));
+        System.out.println(Arrays.toString(twoSum(new int[] {2, 1, 9, 6, 3}, 7)));
+        System.out.println(Arrays.toString(twoSum(new int[] {1, 7, 8, 5, 3}, 6)));
     }
 
     private static boolean hasTwoSumPair(int[] arr, int sum) {
@@ -22,5 +26,21 @@ public class TwoSum {
         }
 
         return false;
+    }
+
+    private static int[] twoSum(int[] nums, int t) {
+        int[] res = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int balance = t - nums[i];
+            if (map.containsKey(balance)) {
+                res[0] = map.get(balance);
+                res[1] = i;
+                break;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return res;
     }
 }
