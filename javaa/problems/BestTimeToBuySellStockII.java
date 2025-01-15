d4be0c49223ffc8c7e686e8df5474103ba21407d
Renish B <renishb10@gmail.com>
2025-01-15 16:26:08 +0530

    best time to buy and sell stock ii

---
+++ javaa/problems/BestTimeToBuySellStockII.java
@@ -0,0 +1,27 @@
+package javaa.problems;
+
+/*
+ * LeetCode: #122 Best Time to Buy and Sell Stock II
+ * 
+ * You are given an integer array prices where prices[i] is the price of a given stock on the ith
+ * day.
+ * 
+ * On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of
+ * the stock at any time. However, you can buy it then immediately sell it on the same day.
+ * 
+ * Find and return the maximum profit you can achieve.
+ */
+public class BestTimeToBuySellStockII {
+    private static int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i - 1] < prices[i])
+                profit += prices[i] - prices[i - 1];
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[] {7, 1, 5, 3, 6, 4}));
+    }
+}
