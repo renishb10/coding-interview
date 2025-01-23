cc80b2eb7a3cbe1cc3e0afe8b353807b06064060
Renish B <renishb10@gmail.com>
2025-01-23 22:25:56 +0530

    merge sorted array starter code

--- javaa/problems/MergeSortedArray.java
+++ javaa/problems/MergeSortedArray.java
@@ -1,5 +1,7 @@
 package javaa.problems;
 
+import java.util.Arrays;
+
 /*
  * LeetCode: #88 - Merge Sorted Array
  * 
@@ -31,5 +33,16 @@ package javaa.problems;
  * nums1. The 0 is only there to ensure the merge result can fit in nums1.
  */
 public class MergeSortedArray {
+    private static void mergeArrays(int[] nums1, int[] nums2) {
 
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = new int[] {1, 2, 3, 0, 0, 0};
+        int[] arr2 = new int[] {2, 5, 6};
+
+        System.out.println(Arrays.toString(arr1));
+        mergeArrays(arr1, arr2);
+        System.out.println(Arrays.toString(arr1));
+    }
 }
