fd58bd5694fa7b81c3311104d287e771ecc5b1fd
Renish B <renishb10@gmail.com>
2025-01-28 23:07:38 +0530

    merge sorted arrays

--- javaa/problems/MergeSortedArray.java
+++ javaa/problems/MergeSortedArray.java
@@ -33,8 +33,20 @@ import java.util.Arrays;
  * nums1. The 0 is only there to ensure the merge result can fit in nums1.
  */
 public class MergeSortedArray {
-    private static void mergeArrays(int[] nums1, int[] nums2) {
-
+    private static void mergeArrays(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        for (int i = nums1.length - 1; i >= 0; i--) {
+            int valAtP1 = p1 >= 0 ? nums1[p1] : Integer.MIN_VALUE;
+            int valAtP2 = p2 >= 0 ? nums2[p2] : Integer.MIN_VALUE;
+            if (valAtP1 > valAtP2) {
+                nums1[i] = valAtP1;
+                p1--;
+            } else {
+                nums1[i] = valAtP2;
+                p2--;
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -42,7 +54,7 @@ public class MergeSortedArray {
         int[] arr2 = new int[] {2, 5, 6};
 
         System.out.println(Arrays.toString(arr1));
-        mergeArrays(arr1, arr2);
+        mergeArrays(arr1, 3, arr2, 3);
         System.out.println(Arrays.toString(arr1));
     }
 }
