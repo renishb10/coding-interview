8ef303c73203b02c470f3941dc64af5a5b30a42a
Renish B <renishb10@gmail.com>
2025-02-04 23:15:54 +0530

    three sum

--- javaa/problems/ThreeSum.java
+++ javaa/problems/ThreeSum.java
@@ -2,7 +2,15 @@ package javaa.problems;
 
 import java.util.HashMap;
 
+/*
+ * LeetCode: #15 Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
+ * such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+ * 
+ * Notice that the solution set must not contain duplicate triplets.
+ */
+
 public class ThreeSum {
+
     public static void main(String[] args) {
         System.out.println("Three Sum");
     }
